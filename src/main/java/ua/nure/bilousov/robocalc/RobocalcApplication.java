f439083688e1f847b09a24b25373d6a81424f15e
Pavlo Bilousov <bilousov.p@dbbest.com>
2021-05-30 17:36:15 +0300

    Initial commit

---
+++ src/main/java/ua/nure/bilousov/robocalc/RobocalcApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.bilousov.robocalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobocalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobocalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/nure/bilousov/robocalc/RobocalcApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.bilousov.robocalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobocalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
