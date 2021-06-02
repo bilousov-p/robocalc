6ac1bcae5f33f7053eaee2b2c5afa76f42a630ce
Pavlo Bilousov <bilousov.p@dbbest.com>
2021-06-02 17:46:01 +0300

    Add calculation of chains size

--- src/main/java/ua/nure/bilousov/robocalc/service/impl/CalculatorServiceImpl.java
+++ src/main/java/ua/nure/bilousov/robocalc/service/impl/CalculatorServiceImpl.java
@@ -11,6 +11,10 @@ import ua.nure.bilousov.robocalc.service.CalculatorService;
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
 
+    private final Double FIRST_PAIR_RATIO = 0.3;
+    private final Double SECOND_PAIR_RATIO = 0.26;
+    private final Double THIRD_PAIR_RATIO = 0.44;
+
     @Override
     public CalculatedParams calculateParameters(InputParams inputParams) {
         CalculatedParams calculatedParams = new CalculatedParams();
@@ -34,18 +38,29 @@ public class CalculatorServiceImpl implements CalculatorService {
             case CYLINDER:
                 manipulatorParams.setNumberOfAngleLinks(1);
                 manipulatorParams.setNumberOfSteadyLinks(1);
+                manipulatorParams.setFirstChainSize(inputParams.getReachZone());
+                manipulatorParams.setSecondChainSize(inputParams.getReachZone());
                 break;
             case PARALLELP:
                 manipulatorParams.setNumberOfAngleLinks(0);
                 manipulatorParams.setNumberOfSteadyLinks(3);
+                manipulatorParams.setFirstChainSize(inputParams.getReachZone());
+                manipulatorParams.setSecondChainSize(inputParams.getReachZone());
+                manipulatorParams.setThirdChainSize(inputParams.getReachZone());
                 break;
             case COMPLEX_SPHERE:
                 manipulatorParams.setNumberOfAngleLinks(3);
                 manipulatorParams.setNumberOfSteadyLinks(0);
+                manipulatorParams.setFirstChainSize(inputParams.getReachZone()*FIRST_PAIR_RATIO);
+                manipulatorParams.setSecondChainSize(inputParams.getReachZone()*SECOND_PAIR_RATIO);
+                manipulatorParams.setThirdChainSize(inputParams.getReachZone()*THIRD_PAIR_RATIO);
                 break;
             case PART_OF_SPHERE:
                 manipulatorParams.setNumberOfAngleLinks(2);
                 manipulatorParams.setNumberOfSteadyLinks(1);
+                manipulatorParams.setFirstChainSize(inputParams.getReachZone());
+                manipulatorParams.setSecondChainSize(inputParams.getReachZone());
+                manipulatorParams.setThirdChainSize(inputParams.getReachZone());
                 break;
             default:
                 manipulatorParams.setNumberOfAngleLinks(0);
